a4d776a54c45f3247d21bce708259a4379e68aa5
github username <github email address>
2018-07-02 16:33:53 -0400

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,7 @@
+class Song < ActiveRecord::Base
+  validates :title, presence: true
+  validates :title, uniqueness: { scope: [:artist_name, :release_year] }
+  validates :released, exclusion: { in: [nil] }
+  validates :release_year, presence: true, if: :released?
+  validates :release_year, inclusion: { within: 0..Time.now.year }, if: :released?
+end
